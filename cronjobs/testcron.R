400b22587cbfeae9da6b8793adcbe4f4f5f95f27
ndeprey <ndeprey@alumni.nd.edu>
2014-08-20 17:12:38 -0400

    changes to test cron job

--- cronjobs/testcron.R
+++ cronjobs/testcron.R
@@ -1,9 +1,11 @@
-df <- iris
+ddate <- Sys.time()
+dddate <- gsub(" ","",ddate)
+newdate <- gsub(":","",dddate)
 
-write.csv(iris,"iris_in_wd.csv")
+write.csv(iris,file=paste(newdate,".csv",sep=""))
 
 try(setwd("/home/developer/MPX/cronjobs/results"))
 
-write.csv(iris, "iris_in_results.csv")
+write.csv(iris, file=paste("in_newdir_",newdate,".csv",sep=""))
 
-getwd()
+
